56aa00483c3122c03d21c1c5882c36924a4baaa6
CHRISTOPHER TRUONG <christophertruongm@gmail.com>
2019-01-22 17:57:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(num)
 
-
+  if num % 5 == 0 && num % 3 == 0
+    return 'FuzzBuzz'
+  end
+  
 end
